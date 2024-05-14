2bc86265f8cefc4f0c3b68af33a4af6f76faad04
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-14 15:12:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from io import BytesIO
 
 import sqlite3
 from imdb import IMDb
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Connect to SQLite database
 conn = sqlite3.connect('movies.db')
@@ -279,21 +281,24 @@ if username:
 
     # Display top genres bar graph
     st.subheader("Top 10 Genres Watched:")
-    genre_names = [genre for genre, _ in top_genres]
-    genre_counts = [num_films for _, num_films in top_genres]
-    st.bar_chart({genre_names[i]: genre_counts[i] for i in range(len(genre_names))})
+    genres = [genre for genre, _ in top_genres]
+    num_films_genre = [num_films for _, num_films in top_genres]
+    plt.barh(genres, num_films_genre)
+    st.pyplot()
 
     # Display top countries bar graph
     st.subheader("Top 10 Countries Watched:")
-    country_names = [country for country, _ in top_countries]
-    country_counts = [num_films for _, num_films in top_countries]
-    st.bar_chart({country_names[i]: country_counts[i] for i in range(len(country_names))})
+    countries = [country for country, _ in top_countries]
+    num_films_country = [num_films for _, num_films in top_countries]
+    plt.barh(countries, num_films_country)
+    st.pyplot()
 
     # Display top languages bar graph
     st.subheader("Top 10 Languages Watched:")
-    language_names = [language for language, _ in top_languages]
-    language_counts = [num_films for _, num_films in top_languages]
-    st.bar_chart({language_names[i]: language_counts[i] for i in range(len(language_names))})
+    languages = [language for language, _ in top_languages]
+    num_films_language = [num_films for _, num_films in top_languages]
+    plt.barh(languages, num_films_language)
+    st.pyplot()
 
 
 
