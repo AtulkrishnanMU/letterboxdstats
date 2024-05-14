3f5766cb68edaa9ecb0ec01fb70828f8841361de
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-14 11:25:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,7 @@ if username:
     
     # Displaying poster images
     if poster_images:
-        for i, image_url in enumerate(poster_images):
-            st.image(image_url, caption=film_names_with_year[i], width=150)
-            if i < len(poster_images) - 1:
-                st.write("")  # Add empty space between images
+        st.image(poster_images, caption=film_names_with_year, width=150)
 
 
 
