a138327b328d31048119803efa8f717af80b6815
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-15 10:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -478,10 +478,11 @@ if username:
         xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color  
         yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color  
         paper_bgcolor='rgba(0, 0, 0, 0)'  # Set paper background color to transparent
+        width=100
     )
     
     # Display the charts
-    st.plotly_chart(fig_bar, use_container_width=True)
+    st.plotly_chart(fig_bar)
     
     
 
