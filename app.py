7a3511ea8707fb0e9354c6c75b60210a688c1e7f
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-15 10:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -477,7 +477,7 @@ if username:
         font=dict(color="black"),
         xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color  
         yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color  
-        paper_bgcolor='rgba(0, 0, 0, 0)'  # Set paper background color to transparent
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
         width=100
     )
     
