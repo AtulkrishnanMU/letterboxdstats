cb5fc1078cc95ef303a80b8183a4dd80ff419621
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-15 10:26:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,7 +403,7 @@ if username:
     # Display top genres bar graph
     genre_counts = count_genre_entries(username) #dictionary of the form {Genre1:count1, Genre2:count2...}
 
-    sorted_genre_counts = sorted(genre_counts.items(), key=lambda x: x[1])[:10]
+    sorted_genre_counts = sorted(genre_counts.items(), key=lambda x: x[1], reverse=False)[:10]
 
     country_counts = get_top_countries(username) #dictionary of the fomr {Country1:count1, Country2:count2...}
 
