fd276f45aade6432cabf6c68c2b020b427bbf9d2
AtulkrishnanMU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-05-15 08:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#page layout
+st.set_page_config(page_title="Analytics", page_icon="🌎", layout="wide")
+
 # Connect to SQLite database
 conn = sqlite3.connect('movies.db')
 c = conn.cursor()
