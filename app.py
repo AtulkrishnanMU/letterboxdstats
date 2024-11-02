995ea450473c841fdcde82b5c9c61ef0191817f9
Atulkrishnan MU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-11-02 23:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ FILE_PATH = "movies.db"
 # Title and logo
 col1, col2 = st.columns([1, 6])  # Create two columns
 with col1:
-    st.image("https://static.wikia.nocookie.net/film-and-television/images/a/a0/Unnamed.png/revision/latest?cb=20221129103323", width=50)  # Logo with a specified width
+    st.image("https://static.wikia.nocookie.net/film-and-television/images/a/a0/Unnamed.png/revision/latest?cb=20221129103323", width=30)  # Logo with a specified width
 
 with col2:
     st.title("Letterboxd User Stats")
